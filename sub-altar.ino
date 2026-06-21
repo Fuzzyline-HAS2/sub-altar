2d45c2a96edaf40c4b1b75c9ec7fb675f64a8bc3
bbangjun360 <jysbruh0602@gmail.com>
2026-06-22 01:47:03 +0900

    Firmware Update v6

--- sub-altar.ino
+++ sub-altar.ino
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2022
  */
 
-#define FIRMWARE_VER 5
+#define FIRMWARE_VER 6
 #include "sub-altar.h"
 
 //************************************************ Core1 ********************************************************************
